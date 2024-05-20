b0653d2ec50eb8b24c7f1a1b8cedb50da90332e4
Gaffar09 <161827872+Gaffar09@users.noreply.github.com>
2024-05-20 13:24:13 +0100

    json files

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require('cypress')
+const {downloadFile} = require('cypress-downloadfile/lib/addPlugin')
+
+module.exports = defineConfig({
+  projectId: 'dqgac3',
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    setupNodeEvents(on, config) {
+         on('task', {downloadFile})
+      }
+    }
+  }
+)
+{
+  "reporter"; "mochawesome",
+  "reporterOptions"; {
+    "charts"; true,
+    "overwrite"; false,
+    "html"; false,
+    "json"; true,
+    "reportDir"; "cypress/report/mochawesome-report"
+   }
+  }
+ 
+
